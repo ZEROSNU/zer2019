c4a7a908f1cd96512273668d5d73654466a87f93
lkh099 <lkh099@snu.ac.kr>
2019-08-23 15:13:58 +0900

    mission_master

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install ros-kinetic-ros-numpy
 sudo apt install ros-kinetic-ompl
-pip2 install pyforms-gui
-pip2 install pyforms-terminal
+pip3 install pyforms-terminal
+pip install pyforms-gui
