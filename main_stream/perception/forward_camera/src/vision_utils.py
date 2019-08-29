54d7d2e533d8d88c89406301e2d7c4689038588a
Sangmin <tkdals9082@naver.com>
2019-08-29 18:31:58 +0900

    vision

--- main_stream/perception/forward_camera/src/vision_utils.py
+++ main_stream/perception/forward_camera/src/vision_utils.py
@@ -3,12 +3,27 @@ import cv2
 import math
 
 # Define values boundaries for color
-lower_yellow = np.array([15,40,150],np.uint8)
-upper_yellow = np.array([40,255,255],np.uint8)
-
+lower_yellow = np.array([5,20,100],np.uint8)
+upper_yellow = np.array([15,255,255],np.uint8)
+    
 lower_green = np.array([80-15,50,50],np.uint8)
 upper_green = np.array([80+15,255,255],np.uint8)
+
+lower_blue = np.array([95,150,150],np.uint8)
+upper_blue = np.array([110,255,255],np.uint8)
+
+lower_white = np.array([200,200,200],np.uint8)
+upper_white = np.array([255,255,255],np.uint8)
+
 #margin = np.ones(4) * 15
+
+def get_hsv_color(event, x, y, flags, param):
+    global hsv
+    global img
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        print("hsv:",hsv[y,x,:])
+        print("rgb:",img[y,x,:])
+
 
 def findColor(hsv_image, lower, upper):
     mask = cv2.inRange(hsv_image, lower, upper)
@@ -27,3 +42,18 @@ def calculate_rsquared(x, y, f):
     else:
         rsquared = 0
     return rsquared
+
+
+if __name__ == "__main__":
+    cv2.namedWindow('img')
+    cv2.setMouseCallback('img',get_hsv_color)
+    img = cv2.imread('/home/kimsangmin/ZERO_VISION/bird/7/192.jpg')
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    blue = findColor(hsv, lower_blue, upper_blue)
+    yellow = findColor(hsv, lower_yellow, upper_yellow)
+    white = findColor(img, lower_white, upper_white)
+    cv2.imshow('img', img)
+    cv2.imshow('blue',blue)
+    cv2.imshow('yellow',yellow)
+    cv2.imshow('white', white)
+    cv2.waitKey(100000)
