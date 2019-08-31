0ddf14a41b5ebc1f5cd50d1fc5c58deda463a05b
snuzero <snu.zero@gmail.com>
2019-08-31 16:46:01 +0900

    fix cam setting, fix port num

--- main_stream/perception/forward_camera/src/fixcam.sh
+++ main_stream/perception/forward_camera/src/fixcam.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+wbt=4500
+exp=4
+wbt_front=4000
+exp_front=3
 v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature_auto=0
-v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature=4000
+v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature=${wbt}
 v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto_priority=0
 v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto=1
-v4l2-ctl -d /dev/video1 --set-ctrl=exposure_absolute=100
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_absolute=${exp}
 
 v4l2-ctl -d /dev/video2 --set-ctrl=white_balance_temperature_auto=0
-v4l2-ctl -d /dev/video2 --set-ctrl=white_balance_temperature=4000
+v4l2-ctl -d /dev/video2 --set-ctrl=white_balance_temperature=${wbt}
 v4l2-ctl -d /dev/video2 --set-ctrl=exposure_auto_priority=0
 v4l2-ctl -d /dev/video2 --set-ctrl=exposure_auto=1
-v4l2-ctl -d /dev/video2 --set-ctrl=exposure_absolute=100
+v4l2-ctl -d /dev/video2 --set-ctrl=exposure_absolute=${exp}
 
 v4l2-ctl -d /dev/video3 --set-ctrl=white_balance_temperature_auto=0
-v4l2-ctl -d /dev/video3 --set-ctrl=white_balance_temperature=4000
+v4l2-ctl -d /dev/video3 --set-ctrl=white_balance_temperature=${wbt_front}
 v4l2-ctl -d /dev/video3 --set-ctrl=exposure_auto_priority=0
 v4l2-ctl -d /dev/video3 --set-ctrl=exposure_auto=1
-v4l2-ctl -d /dev/video3 --set-ctrl=exposure_absolute=100
+v4l2-ctl -d /dev/video3 --set-ctrl=exposure_absolute=${exp_front}
 
 v4l2-ctl -d /dev/video4 --set-ctrl=white_balance_temperature_auto=0
-v4l2-ctl -d /dev/video4 --set-ctrl=white_balance_temperature=4000
+v4l2-ctl -d /dev/video4 --set-ctrl=white_balance_temperature=${wbt}
 v4l2-ctl -d /dev/video4 --set-ctrl=exposure_auto_priority=0
 v4l2-ctl -d /dev/video4 --set-ctrl=exposure_auto=1
-v4l2-ctl -d /dev/video4 --set-ctrl=exposure_absolute=100
+v4l2-ctl -d /dev/video4 --set-ctrl=exposure_absolute=${exp}
