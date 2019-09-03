6d34c57e8e25f6aa3480ec97481ab697732cba10
96lives <96lives@gmail.com>
2019-09-03 22:57:50 +0900

    finished

--- map/process_data.py
+++ map/process_data.py
@@ -6,6 +6,6 @@ out_file = open(out_path, 'w', encoding='utf-8')
 rdr = csv.reader(in_file)
 data = next(rdr)
 for i in range(len(data) // 3):
-    out_file.write("['" + data[3*i] + "' , "  + str(float(data[3*i+1])) + ' , ' + str(float(data[3*i+2])) + ']\n')
+    out_file.write("['" + data[3*i] + "' , "  + str(float(data[3*i+1])) + ' , ' + str(float(data[3*i+2])) + '], \n')
 in_file.close()
 out_file.close()
